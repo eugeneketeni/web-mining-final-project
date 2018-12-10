bed359cfd1c2608c32e5787255d0dc41872fb9e1
Luis Palacios <lpalacio@uncc.edu>
2018-12-09 23:32:52 -0500

    Custom stopwords and links

--- Stopwords.py
+++ Stopwords.py
@@ -1,33 +1,29 @@
 #Importing libraries
 import pandas as pd
-
-data = pd.read_csv("https://raw.githubusercontent.com/eugeneketeni/web-mining-final-project/master/Test_file.csv")
-data.head()
-
-# token
 import nltk, string
 from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 
-table = str.maketrans({key: None for key in string.punctuation})
+#full print display
+pd.set_option('display.max_colwidth', -1)
 
+data = pd.read_csv("https://raw.githubusercontent.com/eugeneketeni/web-mining-final-project/master/Test_file.csv")
+data.head()
 
 data['text'] = data['text'].astype(str).str.lower()
 text = data.loc[:, "text"]
+# filter out punctuation 
+table = str.maketrans({key: None for key in string.punctuation})
 data['text'] = data['text'].apply(lambda x: x.translate(table))
-
+#tokenize
 data['text'] = [word_tokenize(text[i]) for i in range(len(text))]
 
-
+#filter out stop words and custom words
 stopwords = set(stopwords.words('english'))
 customStopWords = set(["rt"])
-
-
-
-pd.set_option('display.max_colwidth', -1)
-
 data['text'] = data['text'].apply(lambda x: [item for item in x if item not in stopwords])
 data['text'] = data['text'].apply(lambda x: [item for item in x if item not in customStopWords])
+#filter out links
 data['text'] = data['text'].apply(lambda x: [item for item in x if not "http" in item])
 
 print(data['text'])
