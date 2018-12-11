089d6e3cffc9269e188e8ffd54c8ef9ccd38e791
Luis Palacios <lpalacio@uncc.edu>
2018-12-11 03:08:18 -0500

    added sensitivity scores for positive or negative based on names. Prints csv with scores

--- Twitter+Data+Analysis.py
+++ Twitter+Data+Analysis.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import nltk
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import nltk, string
 from nltk.stem import WordNetLemmatizer
 from nltk.corpus import wordnet
-
+import csv
 #Test data
 #data = pd.read_csv("https://raw.githubusercontent.com/eugeneketeni/web-mining-final-project/master/Test_file.csv")
 
@@ -14,18 +15,56 @@ from nltk.corpus import wordnet
 data = pd.read_csv("https://raw.githubusercontent.com/eugeneketeni/webmining/master/tweets.csv")
 
 #Turn off output print limit
-#pd.set_option('display.max_colwidth', -1)
+pd.set_option('display.max_colwidth', -1)
+
 
 print(data['text'].head())
-data['text'] = data['text'].astype(str).str.lower()
-text = data.loc[:, "text"]
+data['text'] = data['text'].astype(str)
+
+#Working and Scoring
+analyser = SentimentIntensityAnalyzer()
+def sentiment_analyzer_scores(sentence):
+    score = analyser.polarity_scores(sentence)
+    print("{:-<40} {}".format(sentence, str(score)))
+    return score
+# iterating over rows for specific words to get sensitivity scores (positive or negative) and writing to a csv
+for index, row in data.iterrows():
+    if 'clinton' in row['text']:
+        print(sentiment_analyzer_scores(row['text']))
+        sentiment = sentiment_analyzer_scores(row['text'])
+        pos = sentiment['pos']
+        neg = sentiment['neg']
+        with open('clinton_sentiment.csv','a') as fd:
+            fields = [pos, neg]
+            writer = csv.writer(fd)
+            writer.writerow(fields)
+    if 'trump' in row['text']:
+        print(sentiment_analyzer_scores(row['text']))
+        sentiment = sentiment_analyzer_scores(row['text'])
+        pos = sentiment['pos']
+        neg = sentiment['neg']
+        with open('trump_sentiment.csv','a') as fd:
+            fields = [pos, neg]
+            writer = csv.writer(fd)
+            writer.writerow(fields)
+    if 'russia' in row['text']:
+        print(sentiment_analyzer_scores(row['text']))
+        sentiment = sentiment_analyzer_scores(row['text'])
+        pos = sentiment['pos']
+        neg = sentiment['neg']
+        with open('russia_sentiment.csv','a') as fd:
+            fields = [pos, neg]
+            writer = csv.writer(fd)
+            writer.writerow(fields)
+
+
 table = str.maketrans({key: None for key in string.punctuation})
 stopwords = set(stopwords.words('english'))
 customStopWords = set(["rt",","])
 lemmatizer = WordNetLemmatizer()
 
 def get_wordnet_pos(word):
-    """Map POS tag to first character lemmatize() accepts"""
+    #Map POS tag to first character lemmatize() accepts
     tag = nltk.pos_tag([word])[0][1][0].upper()
     tag_dict = {"J": wordnet.ADJ,
                 "N": wordnet.NOUN,
@@ -53,3 +92,11 @@ def processRow(row):
 
 data['text'] = data['text'].apply(processRow)
 print(data['text'].head())
+
+
+
+
+
+
+
+
